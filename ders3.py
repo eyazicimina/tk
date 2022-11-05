88dd3feec0b16cfc536b2e3cd07345e7383b5406
eyazicimina <94612347+eyazicimina@users.noreply.github.com>
2022-11-05 15:57:28 +0300

    Update ders3.py

--- ders3.py
+++ ders3.py
@@ -1,14 +1,16 @@
-#: Imports
+#: Kütüphaneleri import etme
 import sys
 from OSMPythonTools.overpass import overpassQueryBuilder
 from OSMPythonTools.api import Api
 from OSMPythonTools.nominatim import Nominatim
 from OSMPythonTools.overpass import Overpass
 
+#: Api'leri başlatma
 overpass = Overpass()
 nominatim = Nominatim()
 api = Api()
 
+#
 def özellikÇıkar( nesne: object, hariç: list = [] ) -> dict:
     info = {}
     for a in dir( nesne ):
@@ -35,10 +37,60 @@ def sorgula( girdi, ekstra = 10 ):
             return özellikÇıkar( sonuc, ["timestamp_area_base"] )
     raise Exception("Bilinmeyen format")
 
+print( "1", sorgula(473223865) )
+print( "2", sorgula("Cumhuriyet, Üsküdar") )
+print( "3", sorgula(( 49.4093582, 8.694724 )) )
+print( "4", sorgula(( "Cumhuriyet, Üsküdar", '"highway"="bus_stop"' )) )
 
-print( sorgula(473223865) )
-print( sorgula("Cumhuriyet, Üsküdar") )
-print( sorgula(( 49.4093582, 8.694724 )) )
-print( sorgula(( "Cumhuriyet, Üsküdar", '"highway"="bus_stop"' )) )
+
+s1 = sorgula(473223865)
+s2 = sorgula("Cumhuriyet, Üsküdar")
+s3 = sorgula(( 49.4093582, 8.694724 ))
+s4 = sorgula(( "Cumhuriyet, Üsküdar", '"highway"="bus_stop"' ))
+
+s1 = list(s1.keys())
+s2 = list(s2.keys())
+s3 = list(s3.keys())
+s4 = list(s4.keys())
+
+birleşmiş = s1 + s2 + s3 + s4
+birleşmiş = sorted(birleşmiş)
+yegane = set(birleşmiş)
+
+print(len(yegane))
+
+import collections
+frequency = dict(collections.Counter(birleşmiş))
+print(frequency)
+
+for item in frequency:
+    if frequency[item] == 2:
+        print(item)
+
+
+import pandas as pd
+df = pd.DataFrame(columns = list(yegane))
+
+
+
+df.loc[len(df)] = {s:1 for s in s1}
+df.loc[len(df)] = {s:1 for s in s2}
+df.loc[len(df)] = {s:1 for s in s3}
+df.loc[len(df)] = {s:1 for s in s4}
+print(df)
+
+# df.to_csv("şablon.csv", sep=";")
+
+
+# polygon içindeki...
+# iki nokta arasında
+
+# kaydet
+# oku
+# networkx
+# dönüştür (xml, json, polygon, KML, GML, SHP, OSM)
+# parse (polygon, json, dict ....)
+
+
 
 
