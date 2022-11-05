970a9dae1405c2404f3e4ee064d789e4e09e22f5
eyazicimina <94612347+eyazicimina@users.noreply.github.com>
2022-11-05 11:23:05 +0300

    Update ders3.py

--- ders3.py
+++ ders3.py
@@ -2,51 +2,27 @@
 #: Imports
 import sys
 from OSMPythonTools.api import Api
+from OSMPythonTools.nominatim import Nominatim
 
-#: Create the api
+nominatim = Nominatim()
 api = Api()
-way = api.query('way/680549716')
 
-#: Print the available tags
-print( "TAGS", way.tags() )
-print( "GEOM", way.geometry() )
-print( "ARID", way.areaId() )
-print( "TYID", way.typeId() )
-print( "TIDS", way.typeIdShort() )
-print( "TYPE", way.type() )
-print( "ID  ", way.id() )
-print( "LAT ", way.lat() )
-print( "LON ", way.lon() )
-print( "VISB", way.visible() )
-print( "MEMS", way.members() )
-print( "XML ", way.toXML() )
-print( "CLON", way.centerLon() )
-print( "CLAT", way.centerLat() )
-print( "HIST", way.history())
+def özellikÇıkar( nesne: object, hariç = [] ) -> dict: 
+    çıktı = {}
 
-for n in way.nodes():
-    print( "NODE", n.tags(), n.lat(), n.lon() )
+    return çıktı
 
-print("=========================================================")
+def sorgula( girdi, ekstra = 10 ):
+    if type(girdi) is int:
+        sonuc = api.query(f"way/{girdi}")
+        return özellikÇıkar( sonuc )
 
-from OSMPythonTools.nominatim import Nominatim
-nominatim = Nominatim()
-location = nominatim.query('Cumhuriyet, Üsküdar', wkt=True)
-areaId = location.areaId()
-displayName = location.displayName()
-address = location.address()
-type = location.type()
-print("NAME", displayName)
-print("ARID", areaId)
-print("ADRS", address)
-print("TYPE", type)
-print("WKT ", location.wkt()) 
+    if type(girdi) is str:
+        sonuc = nominatim.query(girdi, wkt=True)
+        return özellikÇıkar( sonuc )
 
-print("=========================================================")
+    if type(girdi) is tuple:
+        sonuc = nominatim.query(girdi[0], girdi[1], reverse=True, zoom= ekstra)
+        return özellikÇıkar( sonuc )
 
-from OSMPythonTools.nominatim import Nominatim
-nominatim = Nominatim()
-location = nominatim.query(49.4093582, 8.694724, reverse=True, zoom=10)
-print("NAME", location.displayName())
-print("ADRS", location.address())
-print("JSON", location.toJSON())
+    raise Exception("Bilinmeyen format")
